d445b8ff98c832c02e1048cbb2a7c90ccaefe092
Stikkerrr <stikkerrrr@gmail.com>
2021-08-24 14:34:01 +0300

    1.8. Состояние объекта #167388 Refactor exchange

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -12,17 +12,17 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        another.load++;
-        load--;
+        another.load += this.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
         Battery batteryBox = new Battery(100);
         Battery myPhone = new Battery(20);
-        for (int i = 0; i < 10; i++) {
-            batteryBox.exchange(myPhone);
-            System.out.println("Заряд батареи смартфона равен: " + myPhone.getLoad());
-            System.out.println("Заряд повербанка равен: " + batteryBox.getLoad());
-        }
+        System.out.println("Заряд батареи смартфона равен: " + myPhone.getLoad());
+        System.out.println("Заряд повербанка равен: " + batteryBox.getLoad());
+        batteryBox.exchange(myPhone);
+        System.out.println("Заряд батареи смартфона равен: " + myPhone.getLoad());
+        System.out.println("Заряд повербанка равен: " + batteryBox.getLoad());
     }
 }
