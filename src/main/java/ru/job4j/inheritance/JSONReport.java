a3ffe22fbaca8a8a38e7c03d99770c85b36e3b43
Stikkerrr <stikkerrrr@gmail.com>
2021-08-30 23:49:53 +0300

    4. Переопределение #174890 #196784 refactor with lineSeparator

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,12 +1,11 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-
     @Override
     public String generate(String name, String body) {
         return "{" + "\n"
-                + "\t\"name\" : \"" + name + "\",\n"
-                + "\t\"body\" : \"" + body + "\"\n"
+                + " \"name\" : \"" + name + "\"," + System.lineSeparator()
+                + " \"body\" : \"" + body + "\"" + System.lineSeparator()
                 + "}";
     }
 }
