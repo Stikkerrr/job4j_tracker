e1772b61c380291009209d9bf6cc99066a5f32bf
Stikkerrr <stikkerrrr@gmail.com>
2021-08-31 00:17:00 +0300

    3. Массивы и модели. #174892

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+        for (int index = 0; index < books.length; index++) {
+            books[index] = new Book();
+        }
+        books[0].setName("Clean code");
+        for (int index = 0; index < books.length; index++) {
+            System.out.println(books[index].getName());
+        }
+        Book temp = books[3];
+        books[3] = books[0];
+        books[0] = temp;
+        for (int index = 0; index < books.length; index++) {
+            System.out.println(books[index].getName());
+        }
+        for (Book book : books) {
+            if (book.getName() != null && book.getName().equals("Clean code")) {
+                System.out.println(book.getName());
+            }
+        }
+    }
+}
