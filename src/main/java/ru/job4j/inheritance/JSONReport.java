1b740314f7301ddea3e2e18d6107c1ecd4b6d976
Stikkerrr <stikkerrrr@gmail.com>
2021-08-31 22:15:41 +0300

    4. Переопределение #174890 #196784 refactor

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "{" + "\n"
+        return "{" + System.lineSeparator()
                 + " \"name\" : \"" + name + "\"," + System.lineSeparator()
                 + " \"body\" : \"" + body + "\"" + System.lineSeparator()
                 + "}";
