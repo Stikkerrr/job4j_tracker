b569ee3d553d3dea4c68c511728a753595e38ef1
Stikkerrr <stikkerrrr@gmail.com>
2021-09-04 18:04:24 +0300

    1. Чтение из консоли. Класс Scanner. #179514

---
+++ src/main/java/ru/job4j/YesNoWTF.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+public class YesNoWTF {
+    private String answer;
+    private boolean forced;
+    private String image;
+
+    public YesNoWTF() {
+
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isForced() {
+        return forced;
+    }
+
+    public void setForced(boolean forced) {
+        this.forced = forced;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
