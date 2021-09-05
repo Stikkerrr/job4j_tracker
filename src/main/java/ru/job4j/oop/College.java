48ad29e37fbf32408e6e9c9c15a6f2e9c3c0bf41
Stikkerrr <stikkerrrr@gmail.com>
2021-09-05 23:46:59 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. #181322 #199314 refactor

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -4,7 +4,7 @@ public class College {
 
     public static void main(String[] args) {
         Freshman vlad = new Freshman();
-        Student student = (Student) vlad;
-        Object obj = (Object) student;
+        Student student = vlad;
+        Object obj = student;
     }
 }
