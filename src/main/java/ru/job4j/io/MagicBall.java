0f7f95668e3c37111cc9833a251b5c3b1b5cd5a9
Stikkerrr <stikkerrrr@gmail.com>
2021-09-05 10:03:48 +0300

    1. Чтение из консоли. Класс Scanner. #179514 #198980 completed according to the task

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -1,42 +1,20 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.Random;
 import java.util.Scanner;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import ru.job4j.YesNoWTF;
 
 public class MagicBall {
 
-    @SuppressWarnings("checkstyle:MissingSwitchDefault")
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://yesno.wtf/api");
         Scanner in = new Scanner(System.in);
-        ObjectMapper mapper = new ObjectMapper();
-        StringBuilder response = new StringBuilder();
-        YesNoWTF result;
         while (!"exit".equals(in.next())) {
             in.nextLine();
-            String inputLine;
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-            try {
-                BufferedReader buffer = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()));
-                while ((inputLine = buffer.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                result = mapper.readValue(response.toString(), YesNoWTF.class);
-                System.out.println(result.getAnswer());
-                response.setLength(0);
-                buffer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            int answer = new Random().nextInt(3);
+            switch (answer) {
+                case 1 -> System.out.println("Да");
+                case 0 -> System.out.println("Нет");
+                default -> System.out.println("Может быть");
             }
         }
     }
