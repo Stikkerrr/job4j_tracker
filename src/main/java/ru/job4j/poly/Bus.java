8a4e992f99d55cba6a32b27c0cf98bf2533b5ae7
Stikkerrr <stikkerrrr@gmail.com>
2021-09-05 20:33:29 +0300

    3. Интерфейс Input #181092

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void setPassengers(int passengers) {
+
+    }
+
+    @Override
+    public int refuel(int price) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    void ride();
+
+    void setPassengers(int passengers);
+
+    int refuel(int price);
+}
