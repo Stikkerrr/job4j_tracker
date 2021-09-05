0bb2ef52de29df2736a3d4a4af3994ebde61eb23
Stikkerrr <stikkerrrr@gmail.com>
2021-09-05 23:31:02 +0300

    3. Интерфейс Input #181092 #199310 refactor

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void ride() {
-
+        System.out.println("Автобус едет по маршруту.");
     }
 
     @Override
     public void setPassengers(int passengers) {
-
+        System.out.println("Количество пассажиров: " + passengers);
     }
 
     @Override
     public int refuel(int price) {
-        return 0;
+        return price;
     }
 }
