5409b9b70129f131b162ad9096503451d72a9dad
Stikkerrr <stikkerrrr@gmail.com>
2021-09-24 13:05:26 +0300

    3. Уникальность элементов и сортировка. #110225

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,13 +1,17 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
+        Comparator<Address> cmpCity = Comparator.comparing(Address::getCity);
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(cmpCity)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -22,9 +22,32 @@ public class ProfilesTest {
         Profiles pr = new Profiles();
         List<Address> rsl = pr.collect(addresses);
         List<Address> expected = List.of(
+                misha,
                 ivan,
-                petr,
-                misha
+                petr
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectWithoutDuplicates() {
+        Address ivan = new Address("Moskva", "Tverskaya", 1, 1);
+        Address petr = new Address("NewYork", "Fulton Street", 2, 1);
+        Address maria = new Address("NewYork", "Fulton Street", 2, 1);
+        Address misha = new Address("Berlin", "Rathausstrabe", 15, 1);
+        List<Profile> addresses = List.of(
+                new Profile(ivan),
+                new Profile(petr),
+                new Profile(maria),
+                new Profile(misha)
+        );
+        Profiles pr = new Profiles();
+        List<Address> rsl = pr.collect(addresses);
+        rsl.forEach(address -> System.out.println(address.getCity()));
+        List<Address> expected = List.of(
+                misha,
+                ivan,
+                petr
         );
         assertThat(rsl, is(expected));
     }
